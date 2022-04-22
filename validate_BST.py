bdc1c231bdf027f1bf37744ed89fee6b515cc1dd
Akhilesh-K-Yadav <akhilesh.sw.engineer@gmail.com>
2022-04-23 01:33:02 +0530

    LeetCoding

---
+++ validate_BST.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/625/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def is_valid(root, left, right):
+            if root is None:
+                return True
+            if not ((root.val > left) and (root.val < right)):
+                return False
+
+            return is_valid(root.left, left, root.val) and is_valid(
+                root.right, root.val, right
+            )
+
+        return is_valid(root, float("-inf"), float("inf"))
